b5fa9706338399d6ef63c46e4a04f60effff174c
jotamontoyo <jotamontoyo@gmail.com>
2016-07-19 19:44:16 +0100

    captcha

--- public/js/captcha.js
+++ public/js/captcha.js
@@ -1,7 +1,7 @@
 
     $(document).ready(function() {
 
-        $('#enviar').prop('disabled', false );
+        $('#enviar').prop('disabled', true );
         $("#captcha-invisible").value = 0;
 
         // Captcha invisible
@@ -20,6 +20,53 @@
                 $("#enviar").prop( "disabled", true );
             };
         });
+
+        var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+        //Generates the captcha function
+        var a = Math.ceil(Math.random() * 9) + '';
+        var b = Math.ceil(Math.random() * 9) + '';
+        var c = char[Math.ceil(Math.random() * 52)] +'';
+        var d = Math.ceil(Math.random() * 9) + '';
+        var e = Math.ceil(Math.random() * 9) + '';
+        var f = char[Math.ceil(Math.random() * 52)] +'';
+
+        var code = a + b + c + d + e + f;
+
+        $('#codigo-captcha').text('Introduce: ' + code);
+
+        var textocaptcha = $('#texto-captcha').val();
+
+        $('#texto-captcha').change(function() {
+            $('#soy-persona').prop("disabled", false);          // deshabilita el boton soy-persona
+            textocaptcha = $('#texto-captcha').val();           // captura el valor introducido
+            if (textocaptcha === code) {
+                $("#enviar").prop("disabled", false);           // si coincide habilita el boton enviar
+                $('#soy-persona').prop("disabled", true);       // deshabilita el boton soy-persona
+            } else {
+                $("#enviar").prop("disabled", true);
+                swal({title: "No coincide", text: "Inténtalo de nuevo", timer: 2000, showConfirmButton: false });
+            };
+        });
+
+
+/*        $('#btn-recargar').click(function() {
+
+            var a = Math.ceil(Math.random() * 9) + '';
+            var b = Math.ceil(Math.random() * 9) + '';
+            var c = char[Math.ceil(Math.random() * 52)] +'';
+            var d = Math.ceil(Math.random() * 9) + '';
+            var e = Math.ceil(Math.random() * 9) + '';
+            var f = char[Math.ceil(Math.random() * 52)] +'';
+
+            var code = a + b + c + d + e + f;
+
+            $('#codigo-captcha').text('Introduce: ' + code);
+
+        }); */
+
 
 
 
