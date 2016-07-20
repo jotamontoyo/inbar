3bfe2568053fb5dda2a34287ab631018f976f01c
jotamontoyo <jotamontoyo@gmail.com>
2016-07-20 19:11:53 +0100

    captcha

--- public/js/captcha.js
+++ public/js/captcha.js
@@ -3,60 +3,48 @@
 
         $('#enviar').prop('disabled', true );
         $("#captcha-invisible").value = 0;
+        $('#texto-captcha').val('');
 
-        $("#captcha-invisible").change(function () {                                // Captcha invisible
+        $("#captcha-invisible").change(function() {                                // Captcha invisible
             if ($(this).length > 0){
-                //qué hacemos si un bot rellena el campo
                 $("#enviar").prop( "disabled", true );
             };
         });
 
-        var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";          // captcha formulario
+        var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",          // captcha formulario
+            code;
 
-        var a = Math.ceil(Math.random() * 9) + '';                                  // genera caracteres
-        var b = Math.ceil(Math.random() * 9) + '';
-        var c = char[Math.ceil(Math.random() * 52)] +'';
-        var d = Math.ceil(Math.random() * 9) + '';
-        var e = Math.ceil(Math.random() * 9) + '';
-        var f = char[Math.ceil(Math.random() * 52)] +'';
+        function obtenerChaptca() {
+            var a = Math.ceil(Math.random() * 9) + '';                                  // genera caracteres
+            var b = Math.ceil(Math.random() * 9) + '';
+            var c = char[Math.ceil(Math.random() * 51)] +'';
+            var d = Math.ceil(Math.random() * 9) + '';
+            var e = Math.ceil(Math.random() * 9) + '';
+            var f = char[Math.ceil(Math.random() * 51)];
+            code = a + b + c + d + e + f;
+            $('#codigo-captcha').text('Introduce: ' + code);
+            return code;
+        };
+        obtenerChaptca();
 
-        var code = a + b + c + d + e + f;
-
-        $('#codigo-captcha').text('Introduce: ' + code);
-
-//        var textocaptcha = $('#texto-captcha').val();
-
-        $('#texto-captcha').change(function() {
-            $('#soy-persona').prop("disabled", false);          // deshabilita el boton soy-persona
-            textocaptcha = $('#texto-captcha').val();           // captura el valor introducido
+        $('#soy-persona').click(function() {
+            $('#soy-persona').prop("disabled", false);                  // habilita el boton soy-persona
+            textocaptcha = $('#texto-captcha').val();                   // captura el valor introducido
             if (textocaptcha === code) {
-                $("#enviar").prop("disabled", false);           // si coincide habilita el boton enviar
-                $('#soy-persona').prop("disabled", true);       // deshabilita el boton soy-persona
+                $("#enviar").prop("disabled", false);                   // si coincide habilita el boton enviar
+                $('#soy-persona').prop("disabled", true);               // deshabilita el boton soy-persona
             } else {
                 $("#enviar").prop("disabled", true);
                 swal({title: "No coincide", text: "Inténtalo de nuevo", timer: 2000, showConfirmButton: false });
-                $('#texto-captcha').text('');
+                $('#texto-captcha').val('');
             };
         });
 
-
-/*        $('#btn-recargar').click(function() {
-
-            var a = Math.ceil(Math.random() * 9) + '';
-            var b = Math.ceil(Math.random() * 9) + '';
-            var c = char[Math.ceil(Math.random() * 52)] +'';
-            var d = Math.ceil(Math.random() * 9) + '';
-            var e = Math.ceil(Math.random() * 9) + '';
-            var f = char[Math.ceil(Math.random() * 52)] +'';
-
-            var code = a + b + c + d + e + f;
-
-            $('#codigo-captcha').text('Introduce: ' + code);
-
-        }); */
-
-
-
-
+        $('#recargar-captcha').click(function() {
+            $("#enviar").prop("disabled", true);
+            $('#soy-persona').prop("disabled", false);          // habilita el boton soy-persona
+            $('#texto-captcha').val('');
+            obtenerChaptca();
+        });
 
     });
